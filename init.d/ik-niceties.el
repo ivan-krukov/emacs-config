2108b99540ff421f026daf37da8502de79356dde
ivan-kryukov <ivan.o.kryukov@gmail.com>
2014-05-31 14:35:10 -0600

    Added git gutter setting.

--- init.d/ik-niceties.el
+++ init.d/ik-niceties.el
@@ -1,6 +1,10 @@
 ;;provide several convenience functions
 ;;The following definitions are here since they do not fit in any other category
 
+					;start in server mode - can connect new clients as I go
 (server-mode t)
+
+					;git
+(global-git-gutter-mode t)
 
 (provide 'ik-niceties)
