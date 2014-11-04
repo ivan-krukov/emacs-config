a4d7484d0d5219638803d7d0094c1562f59b796c
ivan-kryukov <ivan.o.kryukov@gmail.com>
2014-11-04 02:46:55 -0700

    moved the include repos around

--- init.d/ik-packages.el
+++ init.d/ik-packages.el
@@ -3,7 +3,8 @@
 (setq-default package-list '(ace-jump-mode auto-complete-c-headers auto-complete expand-region git-gutter-fringe fringe-helper git-gutter ido-vertical-mode leuven-theme magit git-rebase-mode git-commit-mode monokai-theme org popup smart-mode-line dash tronesque-theme twilight-bright-theme yasnippet iedit))
 
 					; list the repositories containing them
-(setq-default package-archives '(("melpa" . "http://melpa.milkbox.net/packages/") ("http://elpa.gnu.org/packages/")))
+(setq-default package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+								 ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 
 					; activate all the packages (in particular autoloads)
--- init.el
+++ init.el
@@ -28,7 +28,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-term-color-vector [unspecified "#081724" "#ff694d" "#68f6cb" "#fffe4e" "#bad6e2" "#afc0fd" "#d2f1ff" "#d3f9ee"] t)
  '(custom-enabled-themes (quote (leuven)))
- '(custom-safe-themes (quote ("ee6081af57dd389d9c94be45d49cf75d7d737c4a78970325165c7d8cb6eb9e34" "75c9f0b0499ecdd0c856939a5de052742d85af81814e84faa666522c2bba7e85" default)))
+ '(custom-safe-themes (quote ("cb247cf944eea968aa613a5c40f4cb79f4c05503591cf33e5b4224394dd57e94" "ee6081af57dd389d9c94be45d49cf75d7d737c4a78970325165c7d8cb6eb9e34" "75c9f0b0499ecdd0c856939a5de052742d85af81814e84faa666522c2bba7e85" default)))
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
